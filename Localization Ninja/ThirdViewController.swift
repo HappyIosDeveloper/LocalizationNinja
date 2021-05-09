ad39905bd22ff6ad08d64419bea47176c094c060
Alfredo <lovlyboy@gmail.com>
2021-05-09 20:57:35 +0430

    Added lost ThirdViewController class to project.

---
+++ Localization Ninja/ThirdViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.swift
+//  Localization Ninja
+//
+//  Created by Ahmadreza on 5/9/21.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
